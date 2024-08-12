433a859de0c5b8008d33207ab3c3ef1303e049ef
superTyDev <ty.mclyr@gmail.com>
2024-08-11 20:01:43 -0400

    changed trigger style

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,68 +5,61 @@ import { useMediaQuery } from "react-responsive";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-    const systemPrefersDark = useMediaQuery(
-        {
-            query: "(prefers-color-scheme: dark)",
-        },
-        undefined
-    );
+  const systemPrefersDark = useMediaQuery(
+    {
+      query: "(prefers-color-scheme: dark)",
+    },
+    undefined
+  );
 
-    const [settings, setSettings] = useState({});
+  const [settings, setSettings] = useState({});
 
-    useEffect(() => {
-        setSettings({
-            theme: window.localStorage.getItem("theme") || "system",
-            behaviorValue:
-                window.localStorage.getItem("scrollBehavior") || "page",
-            scrollAmount: window.localStorage.getItem("scrollAmount") || 100,
-            lightMusic: window.localStorage.getItem("lightMusic") || false,
-            fitDirection:
-                window.localStorage.getItem("fitDirection") || "height",
-        });
-    }, []);
+  useEffect(() => {
+    setSettings({
+      theme: window.localStorage.getItem("theme") || "system",
+      behaviorValue: window.localStorage.getItem("scrollBehavior") || "page",
+      scrollAmount: window.localStorage.getItem("scrollAmount") || 100,
+      lightMusic: window.localStorage.getItem("lightMusic") || false,
+      fitDirection: window.localStorage.getItem("fitDirection") || "height",
+      lastFolder: window.localStorage.getItem("lastFolder") || "",
+    });
+  }, []);
 
-    useEffect(() => {
-        window.localStorage.setItem("theme", settings.theme);
-        window.localStorage.setItem("scrollBehavior", settings.behaviorValue);
-        window.localStorage.setItem("scrollAmount", settings.scrollAmount);
-        window.localStorage.setItem("lightMusic", settings.lightMusic);
-        window.localStorage.setItem("fitDirection", settings.fitDirection);
+  useEffect(() => {
+    window.localStorage.setItem("theme", settings.theme);
+    window.localStorage.setItem("scrollBehavior", settings.behaviorValue);
+    window.localStorage.setItem("scrollAmount", settings.scrollAmount);
+    window.localStorage.setItem("lightMusic", settings.lightMusic);
+    window.localStorage.setItem("fitDirection", settings.fitDirection);
+    window.localStorage.setItem("lastFolder", settings.lastFolder);
 
-        if (
-            (settings.theme == "system" && systemPrefersDark) ||
-            settings.theme == "dark"
-        ) {
-            document.documentElement.classList.add("theme-dark");
-        } else {
-            document.documentElement.classList.remove("theme-dark");
-        }
+    if (
+      (settings.theme == "system" && systemPrefersDark) ||
+      settings.theme == "dark"
+    ) {
+      document.documentElement.classList.add("theme-dark");
+    } else {
+      document.documentElement.classList.remove("theme-dark");
+    }
 
-        if (settings.lightMusic) {
-            document.documentElement.classList.add("light-music");
-        } else {
-            document.documentElement.classList.remove("light-music");
-        }
-    }, [settings, systemPrefersDark]);
+    if (settings.lightMusic) {
+      document.documentElement.classList.add("light-music");
+    } else {
+      document.documentElement.classList.remove("light-music");
+    }
+  }, [settings, systemPrefersDark]);
 
-    return (
-        <>
-            <Head>
-                <title>Music Reader Connect</title>
-                <meta name="description" content="Music Reader Connect" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
-                <link rel="icon" href="/favicon.png" />
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>Music Reader Connect</title>
+        <meta name="description" content="Music Reader Connect" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
 
-            {/* Router specifies which component to insert here as the main content */}
-            <Component
-                {...pageProps}
-                settings={settings}
-                setSettings={setSettings}
-            />
-        </>
-    );
+      {/* Router specifies which component to insert here as the main content */}
+      <Component {...pageProps} settings={settings} setSettings={setSettings} />
+    </>
+  );
 }
--- pages/readerRolling.jsx
+++ pages/readerRolling.jsx
@@ -12,7 +12,6 @@ import { Document, Page, pdfjs } from "react-pdf";
 
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
-import twitch from "../components/twitch";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
@@ -529,8 +528,8 @@ export default function ReaderRolling({ settings, setSettings }) {
       return false;
     }
 
-    const isLeft = /L{2,}N{2,}L{2,}[NR]$/;
-    const isRight = /R{2,}N{2,}R{2,}[NL]$/;
+    const isLeft = /R{1,}N{0,}L{2,}[NR]$/;
+    const isRight = /L{1,}N{0,}R{2,}[NL]$/;
 
     if (isLeft.test(twitchHistory)) {
       return "mouthPrevious";
@@ -566,7 +565,7 @@ export default function ReaderRolling({ settings, setSettings }) {
       twitchHistory = twitchHistory.replace(/^L+/);
     }
 
-    // console.log(twitchHistory);
+    console.log(twitchHistory);
   };
 
   useEffect(() => {
